be77af6d90487ef3e25e67a0d6e9ce5bc23352c3
chente <plantedcoot706@gmail.com>
2020-10-19 15:32:16 -0500

    lista de valores de x correcta y formato mas ordenado

--- proyectotiroverlet.m
+++ proyectotiroverlet.m
@@ -1,4 +1,5 @@
 clear all
+%-------------------parametros iniciales-----------------
 ts=.1;
 y0=0;
 x0=0;
@@ -6,34 +7,31 @@ t=0:.1:8.4;
 vi= 50;
 ang=60;
 angr=deg2rad(ang);
-vix=vi*cos(angr)
+vix=vi*cos(angr);
 viy=vi*sin(angr);
 densidadDelAire=1.2;
 coeficienteDeArrastre=0.011;
 %densidad de la piedra
 prock=100;
 masa=1;
-volumen=masa/prock
-radio=(3*volumen/(4*pi))^(1/3)
-area=pi*radio^2
-b=0.5*densidadDelAire*coeficienteDeArrastre*area
+volumen=masa/prock;
+radio=(3*volumen/(4*pi))^(1/3);
+area=pi*radio^2;
+b=0.5*densidadDelAire*coeficienteDeArrastre*area;
 
-%s prima de x en al primera posicion o derivada de posicion
-spta=-abs(vix)/vix*b/masa*vix^2
-%xn-1
-xa=x0-vix*ts-(spta*ts^2)
-posicionesEnX=[xa,0]
+%---------------operaciones para determinar valores de x-------------------
 
-%-------------------intento 1
-%posicion en x
-%x=[xa,x0]
-%i = 3:1:90;
-%--------------------intento 2
-x=zeros(1,90)
-x(1)=xa
-x(2)=x0
+%s prima de x en al primera posicion
+spta=-abs(vix)/vix*b/masa*vix^2;
+%posicion xn-1
+xa=x0-vix*ts-(spta*ts^2);
+
+%calculamos y añadimos los valores de x al arreglo de valores en x
+x=zeros(1,90);
+x(1)=xa;
+x(2)=x0;
 for i=3:90
-    x(i)=(2*x(i-1)-x(i-2))-(((x(i-1)-x(i-2))/ts).^2)*(b/masa)*ts^2
+    x(i)=(2*x(i-1)-x(i-2))-(((x(i-1)-x(i-2))/ts).^2)*(b/masa)*ts^2;
     x(i)
 end
 
