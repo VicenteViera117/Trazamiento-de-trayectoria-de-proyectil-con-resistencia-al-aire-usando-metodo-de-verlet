065eb1dcf1f2d49d9ca75e7c76d44c8573780f56
Javier Sanchez Panduro <fj.sanpan@gmail.com>
2020-10-21 09:06:37 -0500

    se agregaron y y velocidad

--- proyectotiroverlet.m
+++ proyectotiroverlet.m
@@ -3,7 +3,7 @@ clc
 %-------------------parametros iniciales-----------------
 ts=.1;
 g = 9.81;
-y0=0;
+y0=10;
 x0=0;
 t=0:.1:8.4;
 vi= 50;
@@ -20,6 +20,12 @@ volumen=masa/prock;
 radio=(3*volumen/(4*pi))^(1/3);
 area=pi*radio^2;
 b=0.5*densidadDelAire*coeficienteDeArrastre*area;
+%-----------------graficamos el volcan-------------------------------------
+%figure('Name','Volcán')
+ciclocos = -pi*20:pi/100:pi*20;
+volcan= y0.*cos(ciclocos/20);
+%plot(ciclocos,volcan)
+hold on
 
 %---------------operaciones para determinar valores de x-------------------
 
@@ -32,9 +38,15 @@ xa=x0-vix*ts-(spta*ts^2);
 x=zeros(1,90);
 x(1)=xa;
 x(2)=x0;
+%velocidad
+vx=zeros(1,90);
+vx(1)=vix;
 for i=3:90
-    x(i)=(2.*x(i-1)-x(i-2))-(((x(i-1)-x(i-2))./ts).^2)*(b/masa).*ts^2;
+    x(i)=(2.x(i-1)-x(i-2))-(((x(i-1)-x(i-2))./ts).^2)(b/masa).*ts^2;
+    vx(i-1)=(x(i)-x(i-1))/ts;
 end
+
+x(1)=x0;
 
 %---------------operaciones para determinar valores de y-------------------
 
@@ -42,24 +54,14 @@ sptay =  -g - abs(viy)/viy* b/masa * viy^2;
 ya = y0 - viy .* ts - sptay .* ts.^2;
 
 % calculamos y añadimos los valores de y
-% y=zeros(1,90);
-% y(1)=ya;
-% y(2)=y0;
-% for i=3:90
-%     y(i)=(2.*y(i-1)-y(i-2))-(((y(i-1)-y(i-2))/ts)^2).*(angr/masa).*ts^2-g.*ts.^2;
-% end
-
-% for i=3:90
-%     y(i)=(2.*y(i-1)-y(i-2))-(((y(i-1)-y(i-2))./ts).^2)*(b/masa).*-(ts^2*g);
-%     y1(i,1) = y(i);
-% end
-
-
-% figure('Name','Volcán')
-% ciclocos = -pi:pi/100:pi;
-% volcan= 2.*cos(ciclocos);
-% plot(ciclocos,volcan)
-
-%xna=x0-ts
-
-
+y=zeros(1,90);
+y(1)=ya;
+y(2)=y0;
+%velocidad
+vy=zeros(1,90);
+vy(1)=viy;
+for i=3:90
+    y(i)=(2.y(i-1)-y(i-2))-(((y(i-1)-y(i-2))./ts).^2)(b/masa)*(ts^2)-(g*ts^2);
+    vy(i-1)=(y(i)-y(i-1))/ts;
+end
+y(1)=y0;
